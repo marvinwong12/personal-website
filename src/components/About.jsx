ee6becf9a6077c33262830d64d61a3ccaf6cca05
marvinwong12 <marvin.cy.wong@gmail.com>
2026-07-31 17:29:28 +0800

    updated intro message

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,19 +14,19 @@ export default function About() {
         <div className="space-y-6">
           <p className="text-lg leading-relaxed text-ink-700">
             I&rsquo;m a current Master of Engineering candidate in Data
-            Science at UCLA, where I haven&rsquo;t just been learning about
-            advanced NLP or massive scale machine learning, but also honing
+            Science at UCLA, where I have not only been learning about
+            advanced NLP and massive scale machine learning, but have also been honing
             my professional skills through product management and financial
-            analysis classes. Prior to this, I had gotten my dual B.S.
+            analysis classes. Prior to this, I earned dual B.S.
             degrees in Data Theory and Atmospheric &amp; Oceanic Sciences,
-            where I learned how to use my data science toolkit in solving
+            where I learned how to use my data science toolkit to solve
             problems across disparate fields.
           </p>
           <p className="text-lg leading-relaxed text-ink-700">
-            Along the way I&rsquo;ve worked as an atmospheric data scientist
+            Along the way I&rsquo;ve picked up valuable skills working as an atmospheric data scientist
             at HKUST and a data analyst intern at PricewaterhouseCoopers.
             Outside of coursework, I&rsquo;m tackling problems that fascinate
-            me. Check out some of what I&rsquo;ve been building below.
+            me. Please check out some of what I&rsquo;ve been building below.
           </p>
         </div>
       </div>
